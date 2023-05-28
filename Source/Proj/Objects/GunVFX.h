52ea53abceface1c42f5b676fcc3a290b8a17b76
Hanuš Valenta <hanus.valenta@protonmail.com>
2023-05-28 15:03:55 +0200

    Does this work?

--- Source/Proj/Objects/GunVFX.h
+++ Source/Proj/Objects/GunVFX.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "DiffUtils.h"
+#include "Proj.h"
 
 #include "GunVFX.generated.h"
 
@@ -8,12 +10,20 @@ UCLASS()
 class UGunEffectBase : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	virtual void SerializeEffect(FArchive &Ar) {};
 };
 
 UCLASS(BlueprintType)
 class UPhysGunEffect : public UGunEffectBase
 {
 	GENERATED_BODY()
+
+	virtual void SerializeEffect(FArchive &Ar) override
+	{
+		Ar << bIsActive;
+	}
 
 public:
 
@@ -23,15 +33,44 @@ public:
 
 using GunEffectVariant = TVariant<bool, UPhysGunEffect*>;
 
-inline FArchive& operator<<(FArchive& Ar, GunEffectVariant& Variant) {
-	if (Variant.GetIndex() == 0)
-	{
-		return Ar;
-	}
+inline FArchive& operator<<(FArchive& Ar, GunEffectVariant& Variant)
+{
+        if (Ar.IsLoading()) {
+	        SIZE_T Index;
 
-	UGunEffectBase* Value = Visit([] (auto& Value)
-	{
-		return reinterpret_cast<UGunEffectBase*>(Value);
-	}, Variant);
-	return Ar << Value;
+        	Ar << Index;
+
+        	Variant = GunEffectVariant{};
+
+                switch (Index)
+        	{
+                case GunEffectVariant::IndexOfType<bool>():
+			ENSURE_TRUE(false, Ar)
+		break;
+                	
+                case GunEffectVariant::IndexOfType<UPhysGunEffect*>():
+                	Variant.Set<UPhysGunEffect*>(NewObject<UPhysGunEffect>());
+		break;
+        	}
+        }
+        else
+        {
+	        if (Variant.GetIndex() == 0)
+	        {
+          		return Ar;
+	        }
+	        
+	        UGunEffectBase* Value = Visit([] (auto& Value)
+	        {
+          		return reinterpret_cast<UGunEffectBase*>(Value);
+	        }, Variant);
+        	
+        	SIZE_T Index = Variant.GetIndex();
+        	
+	        Ar << Index;
+        	
+	        Value->SerializeEffect(Ar);
+	        
+	       return Ar;
+        }
 }
